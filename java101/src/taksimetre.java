2e80851fdbfbf50323003871d1e8f2941cd4d7eb
omerdemirr <omerhbg2@gmail.com>
2026-02-20 17:11:12 +0300

    taksimetre add

---
+++ java101/src/taksimetre.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class taksimetre {
+    public static void main(String[] args) {
+        Scanner input = new Scanner((System.in));
+
+        double KmUcret = 2.20;
+        double Mesafe, Tutar;
+        int min = 20;
+
+        System.out.print("Araç kaç kilometre yol aldı?:");
+        Mesafe = input.nextDouble();
+
+        Tutar = Mesafe * KmUcret;
+
+        if(Tutar >= min){
+            Tutar += 10;
+            System.out.println("Toplam Mesafe : " + Mesafe);
+            System.out.println("Toplam Tutarınız : " + Tutar);
+        }
+        else{
+            Tutar = min;
+            System.out.println("Toplam Tutarınız : " + Tutar);
+        }
+    }
+}
